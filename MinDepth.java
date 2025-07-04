8b375947c3da1c749e52c6d77a6b4a1758bac89d
Harshaeswar <abbireddyharsha18@gmail.com>
2025-07-04 18:58:25 +0530

    Add files via upload

---
+++ MinDepth.java
@@ -0,0 +1,34 @@
+public class MinDepth {
+
+    static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        Node() {}
+        Node(int val) { this.val = val; }
+        Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int minDepth(Node root) {
+        if (root == null) return 0;
+        if (root.left == null) return minDepth(root.right) + 1;
+        if (root.right == null) return minDepth(root.left) + 1;
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        int result = minDepth(root);
+        System.out.println(result);
+    }
+}
