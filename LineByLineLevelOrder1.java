ee01349a353ff2ffc63e9cd014f3d550342d9666
Harshaeswar <abbireddyharsha18@gmail.com>
2025-07-04 18:15:35 +0530

    Add files via upload

---
+++ LineByLineLevelOrder1.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+public class LineByLineLevelOrder1
+{
+	public static void main(String[] args) {
+		Node root= new Node(10);
+		root.left=new Node(20);
+		root.right=new Node(30);
+		root.left.left=new Node(40);
+		root.left.right=new Node(50);
+		root.right.right= new Node(60);
+		levelOrderTraversal(root);
+	}
+    public static void levelOrderTraversal(Node root){
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+           
+            for(int i=0;i<size;i++){
+                 Node curr=q.poll();
+                System.out.print(curr.data+" ");
+                if(curr.left!=null){
+                    q.add(curr.left);
+                }
+                if(curr.right!=null){
+                    q.add(curr.right);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
